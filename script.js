f54beb0431360b13f0eb420a3e2d58865cf58e35
Priscilla Alves <perealvees@gmail.com>
2024-02-20 11:05:22 -0300

    ajuste

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
-function verificar(){
+function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
-    console.log(data)
-    var fano = document.getElementById('dataNasc')
+    var formAno = document.getElementById('dataNasc')
     var res = document.getElementById('res')
 
-    if (fano.value.length ==  0 || parseInt(fano.value) > ano) {
-        alert('#ERRO. Verifique novamente!')
+    if (formAno.value.length == 0 || Number(formAno.value) > ano) {
+        alert('[ERRO] Verifique os dados e tente novamente')
     } else {
         var fsex = document.getElementsByName('radsex')
-        var idade = ano - Number(fano.value)
-        var genero = ''
-        res.innerHTML = `Sua idade é ${idade}`
+        var idade = ano - Number(formAno.value)
+        var genero = ""
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if(fsex[0].checked) {
+            genero = "Homem"
+            if(idade >= 0 && idade < 10) {
+                img.setAttribute("src", "/img/homem criança.png")
+            } else if(idade >= 10 && idade <= 21) {
+                img.setAttribute("src", "/img/menino adolescente.png")
+            } else if (idade < 50) {
+                img.setAttribute("src", "/img/homem adulto.png")
+            } else {
+                img.setAttribute("src", "/img/homem idoso.png")
+            }
+
+        } else if (fsex[1].checked) {
+            genero = "Mulher"
+            if(idade >= 0 && idade < 10) {
+                img.setAttribute("src", "/img/menina criança.png")
+            } else if(idade >= 10 && idade <= 21) {
+                img.setAttribute("src", "/img/menina adolescente.png")
+            } else if (idade < 50) {
+                img.setAttribute("src", "/img/mulher adulta.png")
+            } else {
+                img.setAttribute("src", "/img/mulher idosa.png")
+            }
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
-
-    if(fsex [1].checked){
-        genero = 'Homem'
-    } else if (fsex[0].checked) {
-        genero = 'Mulher'
-    } 
-    res.innerHTML = `Você é do genero ${genero} e sua idade é ${idade}`
 }
-
